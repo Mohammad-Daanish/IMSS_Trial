ca55f875de6981d275fd2d2f19837af0372df584
Mohammad-Daanish <daanishrehan@gmail.com>
2024-07-04 14:29:20 +0530

    This folder for Taj/Daanish Testing

---
+++ Fibonacci_Series.py
@@ -0,0 +1,13 @@
+" WAP for Fibonachi Series "
+number = int(input("Enter a Number for finding Fibonacci: "))
+n1,n2=0,1
+sum=0
+if number<0:
+    print("It Should be greater than zero")
+else:
+    for i in range(0,number):
+        print(sum,end='')
+        n1=n2
+        n2=sum
+        sum=n1+n2
+
